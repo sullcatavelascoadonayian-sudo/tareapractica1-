1336f634dc0c1e7dbd2baccef7b7c27ff5f01442
sullcatavelascoadonayian-sudo <sullcatavelascoadonayian@gmail.com>
2026-09-07 13:36:34 -0400

    Refactor auto class methods and comments

--- tarea1/ejercicio1/auto.java
+++ tarea1/ejercicio1/auto.java
@@ -1,30 +1,28 @@
 public class auto {
-    // Atributos de la clase
     private String marca;
     private String modelo;
     private int anio;
     private int kilometraje;
-    private String color; // Se agrega para el inciso b
-    // Constructor para inicializar el vehículo
+    private String color; 
     public auto(String marca, String modelo, int anio, int kilometraje) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
         this.kilometraje = kilometraje;
-        this.color = "Sin color"; // Color por defecto
+        this.color = "Sin color"; 
     }
-    // a) Método para mostrar el kilometraje en kilómetros y metros
+    // a) mostrar el kilometraje en kilómetros y metros
     public void mostrarKilometraje() {
         int metros = this.kilometraje * 1000;
         System.out.println("Vehículo: " + marca + " " + modelo + " (" + color + ")");
         System.out.println("Kilometraje: " + kilometraje + " km (" + metros + " metros)");
         System.out.println("------------------------------------");
     }
-    // b) Método para cambiar el color del auto
+    // b)cambiar el color del auto
     public void cambiarColor(String nuevoColor) {
         this.color = nuevoColor;
     }
-    // c) Método main donde creamos dos autos y probamos los métodos
+    // c)  dos autos y probamos los métodos
     public static void main(String[] args) {
         auto auto1 = new auto("Toyota", "Corolla", 2020, 150);
         auto auto2 = new auto("Nissan", "Sentra", 2018, 85);
